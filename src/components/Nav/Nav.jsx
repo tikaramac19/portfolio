7ab5e96bb449c26822509f271602b84d634051e1
tikaramac19 <tikaramac19@gmail.com>
2022-11-24 14:00:01 +0545

    fixed navbar

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceFill } from "react-icons/ri";
+import { AiOutlineMessage } from "react-icons/ai";
+import "./Nav.css";
+import { useState } from "react";
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <RiServiceFill />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <AiOutlineMessage />
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
