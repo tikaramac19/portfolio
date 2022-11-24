e341b8b1f49b9769db94dcf0cc5a75e00a4491fa
tikaramac19 <tikaramac19@gmail.com>
2022-11-24 22:18:11 +0545

    services section removed

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -31,9 +31,9 @@ const Nav = () => {
         <BiBook />
       </a>
       <a
-        href="#services"
-        onClick={() => setActiveNav("#services")}
-        className={activeNav === "#services" ? "active" : ""}
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
       >
         <RiServiceFill />
       </a>
