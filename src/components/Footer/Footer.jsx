144411957ed3c78b21b42d568bd4bd7ecbb969d1
tikaramac19 <tikaramac19@gmail.com>
2022-11-25 15:22:13 +0545

    footer complited

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
+import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="" className="footer-logo">
+        Tikaram Ac
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="social-footer">
+        <a href="https://facebook.com">
+          <BsFacebook />
+        </a>
+        <a href="https://instagram.com">
+          <BsInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <BsTwitter />
+        </a>
+      </div>
+      <div className="footer-copyright">
+        <small>&copy; Tikaram Ac. All rights reserved. </small>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
